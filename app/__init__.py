0a1344925b4cb41fe85ec4c0e64627bf65b49b79
Clayt <clayton.wwjd@gmail.com>
2020-02-07 23:36:35 -0600

    Added further basic structure to service

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,7 @@ from flask import flask
 
 app = Flask(__name__)
 
+from app import routes
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    pass
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    pass
+
+
+@app.route('/register',methods=['POST'])
+def register():
+    pass
