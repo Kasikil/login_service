148558268ac291340d99b05ba5a9a5f6b06e5291
Clayt <clayton.wwjd@gmail.com>
2020-02-07 23:36:57 -0600

    Added unit test framework without implementation

---
+++ tests.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+import unittest
+from app import app
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_feature(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
