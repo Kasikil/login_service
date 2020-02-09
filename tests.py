3167790a44a3e7294cd519998192975a0f1f76e5
Clayt <clayton.wwjd@gmail.com>
2020-02-08 21:19:50 -0600

    Added a bit more logic, still no passing functionality

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
-from datetime import datetime, timedelta
-import unittest
 from app import app
+from datetime import datetime, timedelta
+from flask_api import status
+import unittest
+
 
 class UserModelCase(unittest.TestCase):
     # Special method
@@ -12,8 +14,21 @@ class UserModelCase(unittest.TestCase):
         pass
     
     # Standard method
-    def test_feature_example(self):
+    def test_feature_example_pass_or_fail(self):
         pass
+
+    def test_login_success(self):
+        good_creds = {'username': 'Psweet', 'password': 'dollas'}
+        with app.test_client() as client:
+            response = client.post('/login', data=good_creds)
+            self.assertTrue(status.is_success(response.status_code))
+
+    # Test several of these methods for different failure cases
+    def test_login_failure(self):
+        bad_creds = {}
+        with app.test_client() as client:
+            response = client.post('/login', data=bad_creds)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
