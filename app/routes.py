dbe096016628c6e03de9f50c0ceb707d943f61d1
Clayt <clayton.wwjd@gmail.com>
2020-02-08 21:19:29 -0600

    Minor updates to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import request
+from flask_api import status
 
 
 @app.route('/login', methods=['POST'])
@@ -7,12 +8,13 @@ def login():
     data = request.get_json(force=True)
     data['username']
     data['password']
-    # Sanatize the username and password at this step to prevent SQL INJECTION ATTACKS
+    # Sanitize the username and password at this step to prevent SQL INJECTION ATTACKS
     # Query the database here for the username and its associated password
     # If the username does not exist, return 400 {with username error message}
     # If the username does exist, check if the passwords match
     # If the passwords do not match, return 400 {with password error message}
     # If the passwords do match, return 200 {with a login cookie to bind to the client}
+    # return {'username': data['username'], 'password': data['password']}, status.HTTP_200_OK
     pass
 
 
@@ -29,7 +31,7 @@ def register():
     data['username']
     data['password']
     data['email']
-    # Sanatize all inputs to prevent SQL INJECTION ATTACKS
+    # Sanitize all inputs to prevent SQL INJECTION ATTACKS
     # Query the database to check if username exists
     # If username exists, return 400 {with username taken error message}
     # If username does not exist, attempt to save data to the database
