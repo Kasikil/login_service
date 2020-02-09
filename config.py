5027db7e866da2124c21650c0a4b8c35e2cbfcbb
Clayt <clayton.wwjd@gmail.com>
2020-02-09 00:02:55 -0600

    Updated config to be a Python class with attributes

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-class Config(object):
-    database_url = ''
+class Config:
     database_name = ''
+    database_url = 'http://strumdelicious.com/'
     database_password = ''
+    # Testing parameters
+    good_test_user = 'Psweet'
+    good_test_password = 'dollas'
+    bad_test_user = 'Kasi'
+    bad_test_password = 'Chapped'
